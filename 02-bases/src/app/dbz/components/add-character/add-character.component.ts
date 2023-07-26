283bbd8f98eb1bdbad318199203f54357b3e5564
JcAmela <juancarlosamela@gmail.com>
2023-07-26 12:57:03 +0200

    output

--- 02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCharacterComponent {
 
+  public character: Character={
+    name:'',
+    power:0,
+  }
 
+  emitCharacter():void{
+
+    console.log(this.character)
+
+    this.character.name = '';
+    this.character.power = 0;
+  };
 
 }
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -16,7 +16,11 @@ export class MainPageComponent {
         {
             name:'Goku',
             power:9500,
-        }
+        },   
+        {
+            name:'Vegeta',
+            power:7500,
+        },
     ]
 
 }
